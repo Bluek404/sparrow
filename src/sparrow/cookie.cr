5c5dc12b98769b6b185042a74b1112c5b797618c
Bluek404 <i@bluek404.net>
2015-06-27 00:57:00 +0800

    实验性cookie支持

---
+++ src/sparrow/cookie.cr
@@ -0,0 +1,61 @@
+class HTTP::Request
+  @cookie = Hash(String, String).new
+  def initialize(@method : String, @path, @headers = Headers.new : Headers, @body = nil, @version = "HTTP/1.1")
+    if body = @body
+      @headers["Content-length"] = body.bytesize.to_s
+    elsif @method == "POST" || @method == "PUT"
+      @headers["Content-length"] = "0"
+    end
+    parse_cookie()
+  end
+
+  private def parse_cookie()
+    if @headers.has_key?("Cookie")
+      # TODO: ...
+    end
+  end
+
+  def cookie
+    @cookie
+  end
+end
+
+class HTTP::Cookie < Hash(String, String)
+  def initialize(@response)
+    super()
+  end
+
+  def []=(key : String, value : String)
+    # TODO: 转码
+    cookie = "#{ key }=#{ value };"
+    if @response.headers.has_key?("Set-cookie")
+      @response.headers["Set-cookie"] += cookie
+    else
+      @response.headers["Set-cookie"] = cookie
+    end
+    super
+  end
+end
+
+class HTTP::Response
+  # 因为下面的init方法是覆盖的，所以无法在所有初始化方法里初始@cookie，只能预先初始占位
+  @cookie = Hash(String, String).new
+
+  def initialize(@status_code, @body = nil, @headers = Headers.new : Headers, status_message = nil, @version = "HTTP/1.1")
+    @status_message = status_message || self.class.default_status_message_for(@status_code)
+
+    if (body = @body)
+      @headers["Content-length"] = body.bytesize.to_s
+    end
+
+    init_cookie() # 真正初始化@cookie
+  end
+
+  private def init_cookie()
+    @cookie = Cookie.new(self)
+  end
+  def cookie
+    @cookie
+  end
+end
+
--- src/sparrow/handler.cr
+++ src/sparrow/handler.cr
@@ -1,7 +1,23 @@
 module Sparrow::Handler
   extend self
 
+  private def new_id()
+    "test"
+  end
+  private def check_cookie(request, response)
+    pp request.headers
+    if request.cookie.has_key?("key")
+      # ...
+    else
+      id = new_id()
+      response.cookie["id"] = id
+    end
+    pp response.headers
+  end
+
   def home(request)
-    HTTP::Response.ok("text/html", View::Home.new.to_s)
+    response = HTTP::Response.new(200, View::Home.new.to_s)
+    check_cookie(request, response)
+    response
   end
 end
